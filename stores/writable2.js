36ea556e045e7db01788919fb109d37a3b0e852b
Grzegorz (Greg) Twardowski <tvardy@users.noreply.github.com>
2023-01-15 15:17:20 +0000

    writeable2 update

--- stores/writable2.js
+++ stores/writable2.js
@@ -8,11 +8,15 @@ export function writable2(value, start) {
     let {set, ...rest} = writable(value, start);
 
     return {
+        ...rest,
         set: value => {
             storeValue = value;
             set(value);
         },
+        update: fn => {
+            storeValue = fn(value);
+            set(storeValue);
+        },
         get: () => storeValue,
-        ...rest
     };
 }
